747120cf81196a0395104bf0d4b87e5e5694577b
guiyukun <guiyukun@meituan.com>
2020-09-14 16:29:37 +0800

    styles: add font&icon files

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 引入vue模块
 import Vue from 'vue'
+// 引入vue组件
 import App from './App'
+// 引入路由配置文件
 import router from './router'
 
+// 关闭生产模式下给的提示
 Vue.config.productionTip = false
 
+// 创建一个vue的根实例
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  router, // 注入路由配置
+  components: { App }, // 配置根模版，即打开页面显示的组件
+  template: '<App/>' // 注入组件
 })
